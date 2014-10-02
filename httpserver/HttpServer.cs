d963f2f4d4ba38caa0792130709c57cf74ea2d84
mubaslam <mubaslam@gmail.com>
2014-10-02 23:00:19 +0200

    Worked out some flaws... commencing step 4: Threading...

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -13,62 +13,76 @@ namespace httpserver
     public class HttpServer
     {
         public static readonly int DefaultPort = 8888;
+
         private bool serverOn;
 
         private static readonly string RootCatalog = @"c:/temp";
 
-       public void StartServer()
-       {
-           serverOn = true;
+        public void StartServer()
+        {
+            serverOn = true;
 
-            TcpListener goListener = new TcpListener(DefaultPort);
-            goListener.Start();
+            while (serverOn)
+            {
+                TcpListener goListener = new TcpListener(DefaultPort);
+                goListener.Start();
 
-           while (serverOn)
-           {
-               TcpClient goTcpClient = goListener.AcceptTcpClient();
-               Console.WriteLine("Server Activated");
+                TcpClient goTcpClient = goListener.AcceptTcpClient();
+                Console.WriteLine("Server Activated");
 
-               Stream ns = goTcpClient.GetStream();
-               StreamReader sr = new StreamReader(ns);
-               try
-               {
-                   StreamWriter sw = new StreamWriter(ns);
-                   sw.AutoFlush = true; //enables automatic flushing
-                   string message = sr.ReadLine();
-                   string[] words = message.Split(' ');
-                   string filePath = RootCatalog + words[1];
+                Stream ns = goTcpClient.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                try
+                {
+                    StreamWriter sw = new StreamWriter(ns);
+                    sw.AutoFlush = true; //enables automatic flushing
+                    string message = sr.ReadLine();
+                    string answer = "";
+                    string[] words = message.Split(' ');
+                    foreach (string word in words)
+                    {
+                        Console.WriteLine(word);
+                    }
+                    string filePath = RootCatalog + words[1];
+                    
 
-                   if (!File.Exists(filePath))
-                   {
-                       sw.Write("HTTP//1.0 404 Not Found\r\n\r\n");
-                   }
-                   else
-                   {
-                       FileStream source = File.Open(filePath, FileMode.Open, FileAccess.Read);
-                       source.CopyTo(sw.BaseStream);
-                       source.Flush();
-                   
-                   Console.WriteLine(message);
-               }
-               sr.Close();
-           }
-           catch (Exception)
-           {
-               Console.WriteLine("Unidentified Error");
-           }
-           finally 
-           {
-                ns.Close();
-                goTcpClient.Close();
+                    if (!File.Exists(filePath))
+                    {
+                        sw.Write("HTTP/1.0 404 Not Found\r\n\r\n");
+                    }
+                    else
+                    {
+                        //FileInfo fi = new FileInfo(filePath);
+
+                        answer = "HTTP/1.0 200 OK\r\n\r\n";
+                        sw.WriteLine(answer);
+                        message = sr.ReadLine();
+                        using (FileStream source = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                        {
+                            source.CopyTo(sw.BaseStream);
+                            source.Flush();
+                        }
+                        Console.WriteLine(message);
+                    }
+                    sr.Close();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Unidentified Error");
+                }
+                finally
+                {
+                    ns.Close();
+                    goTcpClient.Close();
+                    goListener.Stop();
                 }
             }
-            goListener.Stop();
-        }
-           
-        
-          
-
         }
     }
+}
+            
+            
+        
+           
+    
 
